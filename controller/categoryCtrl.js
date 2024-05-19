2e9a4524bebd6f6fa2d1a77a298146216eba04e5
Muhammad Talha <talhamushtaq73@gmail.com>
2024-05-20 01:15:04 +0500

    Getting Categories Added

--- controller/categoryCtrl.js
+++ controller/categoryCtrl.js
@@ -23,4 +23,36 @@ const updateCategory = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports = {createCategory, updateCategory}
+const deleteCategory = asyncHandler(async (req, res) => {
+    const {id} = req.params;
+    validateMongoDbId(id);
+    try{
+        const deleteCategory = await Category.findByIdAndDelete(id)
+        res.json(deleteCategory);
+
+    }catch(error){
+        throw new Error(error)
+    }
+})
+
+const getaCategory = asyncHandler(async (req, res) => {
+    const {id} = req.params;
+    validateMongoDbId(id);
+    try{
+        const findCategory = await Category.findById(id);
+        res.json(findCategory)
+    }catch(error){
+        throw new Error(error)
+    }
+})
+
+const getallCategory = asyncHandler(async (req, res) => {
+    try{
+        const findCategory = await Category.find();
+        res.json(findCategory)
+    }catch(error){
+        throw new Error(error)
+    }
+})
+
+module.exports = {createCategory, updateCategory, deleteCategory, getaCategory, getallCategory}
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { createCategory, updateCategory} = require('../controller/categoryCtrl');
+const { createCategory, updateCategory, deleteCategory, getaCategory, getallCategory} = require('../controller/categoryCtrl');
 const {isAdmin, authMiddleware} = require('../middlewares/authMiddleware');
 
 router.post('/',authMiddleware, isAdmin, createCategory)
 router.put('/:id',authMiddleware, isAdmin, updateCategory)
+router.delete('/:id',authMiddleware, isAdmin, deleteCategory)
+router.get('/getall', getallCategory)
+router.get('/:id', getaCategory)
+
 
 module.exports = router;
