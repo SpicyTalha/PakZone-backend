04612fc570d24a7b9ffb3e75ad351f05906cfdcc
Muhammad Talha <talhamushtaq73@gmail.com>
2024-05-19 19:40:58 +0500

    Added a Create Product and Getting Products API's

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -2,9 +2,31 @@ const Product = require('../models/productModel');
 const asyncHandler = require('express-async-handler');
 
 const createProduct = asyncHandler(async (req, res) => {
-    res.json({
-        msg: "hi product page"
-    })
+    try{
+        const newProduct = await Product.create(req.body);
+        res.json(newProduct)
+    }catch(error){
+        throw new Error(error)
+    }
 })
 
-module.exports = {createProduct}
+const getaProduct = asyncHandler(async (req, res) => {
+    const {id} = req.params;
+    try{
+        const findProduct = await Product.findById(id);
+        res.json(findProduct)
+    }catch(error){
+        throw new Error(error)
+    }
+})
+
+const getallProduct = asyncHandler(async (req, res) => {
+    try{
+        const allProduct = await Product.find();
+        res.json(allProduct)
+    }catch(error){
+        throw new Error(error)
+    }
+})
+
+module.exports = {createProduct, getaProduct, getallProduct}
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createProduct } = require('../controller/productCtrl');
+const { createProduct, getaProduct, getallProduct } = require('../controller/productCtrl');
 
 router.post('/', createProduct)
+router.get('/:id', getaProduct)
+router.get('/', getallProduct)
 
 module.exports = router;
