ab1d64f270eb68023e00326f4d06a44d0372f7b5
Muhammad Talha <talhamushtaq73@gmail.com>
2024-05-19 20:52:48 +0500

    Added Filtering, Pagination, Limiting and Sorting when Getting Products

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const Product = require('../models/productModel');
 const asyncHandler = require('express-async-handler');
 const slugify = require('slugify');
@@ -51,8 +52,47 @@ const getaProduct = asyncHandler(async (req, res) => {
 
 const getallProduct = asyncHandler(async (req, res) => {
     try{
-        const allProduct = await Product.find();
-        res.json(allProduct)
+        //Filtering
+        const queryObj = {...req.query}
+        const excludeFields = ["page", "sort", "limit", "fields"]
+        excludeFields.forEach(el =>{
+            delete queryObj[el]
+        })
+
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
+
+        let query = Product.find(JSON.parse(queryStr))
+        
+        //sorting
+        if (req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ')
+            query.sort(sortBy)
+        }
+        else{
+            query = query.sort('-createdAt')
+        }
+
+        //limiting
+        if (req.query.fields){
+            const fields = req.query.fields.split(',').join(' ')
+            query = query.select(fields)
+        }
+        else{
+            query = query.select("-__v")
+        }
+        //pagination
+        const page = req.query.page;
+        const limit = req.query.limit;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit)
+        if(req.query.page){
+            const productCount = await Product.countDocuments();
+            if (skip >= productCount) throw new Error("This page does not exist")
+        }
+
+        const product = await query;
+        res.json(product)
     }catch(error){
         throw new Error(error)
     }
